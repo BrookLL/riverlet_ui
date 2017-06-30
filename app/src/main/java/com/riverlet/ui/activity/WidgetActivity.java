cc204481cf95e4a4042eb129d880b176ad2b28fa
liujian <liujian@ecarx.com.cn>
2017-06-30 11:16:51 +0800

    feat: 完成了RingView的展示

--- app/src/main/java/com/riverlet/ui/activity/WidgetActivity.java
+++ app/src/main/java/com/riverlet/ui/activity/WidgetActivity.java
@@ -3,6 +3,7 @@ package com.riverlet.ui.activity;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 
 import com.liu.riverlet.R;
 import com.riverlet.ui.base.BaseActivity;
@@ -16,9 +17,10 @@ import java.util.List;
  * Created by liujian on 17-6-29.
  */
 
-public class WidgetActivity extends BaseActivity {
+public class WidgetActivity extends BaseActivity implements View.OnClickListener {
     private List<Fragment> fragmentList = new ArrayList<>();
     private ViewPager viewPager;
+    private List<View> viewList = new ArrayList<>();
     @Override
     protected int getLayoutId() {
         return R.layout.activity_widget;
@@ -27,11 +29,17 @@ public class WidgetActivity extends BaseActivity {
     @Override
     protected void initView() {
         viewPager = (ViewPager) bindId(R.id.viewPager);
+        RingView ringView = new RingView(this);
+        ringView.setOnClickListener(this);
+        ringView.setData(new float[]{100f,100f,100f,100f,100f});
+        viewList.add(ringView);
     }
 
     @Override
     protected void initData() {
-        fragmentList.add(WidgetFragment.newInstance(new RingView(this)));
+        for (View view : viewList) {
+            fragmentList.add(WidgetFragment.newInstance(view));
+        }
         viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
             @Override
             public Fragment getItem(int position) {
@@ -43,5 +51,12 @@ public class WidgetActivity extends BaseActivity {
                 return fragmentList.size();
             }
         });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view instanceof RingView){
+            ((RingView) view).animStart();
+        }
     }
 }
